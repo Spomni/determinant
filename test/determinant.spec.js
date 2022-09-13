659238aac1fb73990afbdd370e8ade29d5633894
spomni <mikhail.snitko@gmail.com>
2022-09-13 13:09:46 +0300

    test: describe test cases

---
+++ test/determinant.spec.js
@@ -0,0 +1,16 @@
+const { assert } = require('chai')
+
+describe('determinant():', () => {
+
+  describe('should throw an error if the passed value is not a square matrix of numbers', () => {
+    it('passed value is not matrix')
+    it('passed matrix is not square')
+    it('any item of passed matrix is not number')
+  })
+  
+  describe('should calculate determinant of the matrix:', () => {
+    it('2x2')
+    it('3x3')
+    it('any size')
+  })
+})
