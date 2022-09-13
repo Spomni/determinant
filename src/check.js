3722d651f645e7cde6eb7ae126a7f03f32fe443a
spomni <mikhail.snitko@gmail.com>
2022-09-13 15:15:42 +0300

    refactor: move check functions to the separate file

---
+++ src/check.js
@@ -0,0 +1,64 @@
+function isArray(value) {
+  return value instanceof Array
+}
+
+function isMatrix(value) {
+
+  // is array 
+  if (!isArray(value)) return false
+
+  // array items are arrays
+  for (let i=0; i<value.length; i++) {
+    const item = value[i]
+    if (!isArray(item)) return false
+  }
+
+  // inner arrays have equal lengths
+  const length = value[0].length
+
+  for (let i=0; i<value.length; i++) {
+    const item = value[i]
+    if (item.length !== length) return false
+  }
+  
+  // true
+  return true
+}
+
+function isSquareMatrix(matrix) {
+
+  const m = matrix.length
+  const n = matrix[0].length
+
+  return m === n
+}
+
+function isMatrixOfNumbers(matrix) {
+  
+  const m = matrix.length
+  const n = matrix[0].length
+
+  for (let i=0; i<m; i++) {
+    for (let j=0; j<n; j++) {
+      if (typeof matrix[i][j] !== 'number') return false
+    }
+  }
+  
+  return true
+}
+
+function isSquareMatrixOfNumbers(value) {
+  if (
+    isMatrix(value) &&
+    isSquareMatrix(value) &&
+    isMatrixOfNumbers(value)
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+module.exports = {
+  isSquareMatrixOfNumbers,
+}
--- src/determinant.js
+++ src/determinant.js
@@ -1,71 +1,13 @@
-function isArray(value) {
-  return value instanceof Array
-}
-
-function isMatrix(value) {
-
-  // is array 
-  if (!isArray(value)) return false
-
-  // array items are arrays
-  for (let i=0; i<value.length; i++) {
-    const item = value[i]
-    if (!isArray(item)) return false
-  }
-
-  // inner arrays have equal lengths
-  const length = value[0].length
-
-  for (let i=0; i<value.length; i++) {
-    const item = value[i]
-    if (item.length !== length) return false
-  }
-  
-  // true
-  return true
-}
-
-function isSquareMatrix(matrix) {
-
-  const m = matrix.length
-  const n = matrix[0].length
-
-  return m === n
-}
-
-function isMatrixOfNumbers(matrix) {
-  
-  const m = matrix.length
-  const n = matrix[0].length
-
-  for (let i=0; i<m; i++) {
-    for (let j=0; j<n; j++) {
-      if (typeof matrix[i][j] !== 'number') return false
-    }
-  }
-  
-  return true
-}
+const { isSquareMatrixOfNumbers } = require('./check')
 
 class InvalidMatrixError extends TypeError {
   constructor() {
-    super('Passed value must be a square matric of numbers')
-  }
-}
-
-function isSquareMatrixOfNumbers(value) {
-  if (
-    isMatrix(value) &&
-    isSquareMatrix(value) &&
-    isMatrixOfNumbers(value)
-  ) {
-    return true
-  } else {
-    return false
+    super('Passed value must be a square matrix of numbers')
   }
 }
 
 function determinant(matrix) {
+
   if (!isSquareMatrixOfNumbers(matrix)) {
     throw new InvalidMatrixError()
   }
